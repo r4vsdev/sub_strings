3ff9770ecb252e3bf95e2dea0c74ae6aaf5b8ea3
r4vsdev <r4vsdev@gmail.com>
2022-10-18 17:45:21 -0300

    Remove unnecessary code

--- substrings.rb
+++ substrings.rb
@@ -1,17 +1,11 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-
-def count_em(str, substr)
-  str.scan(substr).count
-end
-
 def substrings(phrase, dictionary)
-  processed_phrase = phrase.split.map {|item| item.tr(',.?!\'', '')}.join(' ').downcase
-  included_substrings =  dictionary.select {|substring| processed_phrase.include?(substring)}
-  included_substrings.reduce(Hash.new(0)) do |obj, item|
-    obj[item] = count_em(processed_phrase, item)
+  included_substrings = dictionary.select {|substring| phrase.downcase.include?(substring)}
+  included_substrings.reduce(Hash.new(0)) do |obj, substring|
+    obj[substring] = phrase.downcase.scan(substring).count
     obj
   end
 end
 
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
 p substrings("Howdy partner, sit down! How's it going?", dictionary)
-p substrings("below", dictionary)
